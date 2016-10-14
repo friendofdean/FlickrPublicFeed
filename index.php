8d446e1b2137a638b44f3c2d55bd140eb1f08ad0
friendofdean <connoraustin@sky.com>
2016-10-14 14:27:37 +0100

    Initial Upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta  name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FE Dev Test</title>
+        <link rel="stylesheet" type="text/css" href="style/main.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" type='text/javascript'></script>
+        <script src="scripts/main.js" type='text/javascript'></script>
+    </head>
+    <body>
+        <h1>Flickr Public Feed</h1>
+        <div id="feed">
+            <ul id="feed-list">
+            </ul>
+        </div>
+    </body>
+</html>
