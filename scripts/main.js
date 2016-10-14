b024208533baaa08b259cd4c26aaf5033916de33
friendofdean <connoraustin@sky.com>
2016-10-14 14:54:52 +0100

    Add files via upload
    Layout changes when width is below 800px

--- scripts/main.js
+++ scripts/main.js
@@ -4,39 +4,45 @@ $(document).ready(function() {
 
 var wideText =      '<li class="feedItem row">' +
                     '<div class="post-image col-2">\n' +
-                    '<img src="img/Connor.png" alt="No Image" />\n' +
+                    '<a href=""><img src="" alt="No Image" /></a>\n' +
                     '<\/div>\n' +
                     '<div class="post-details col-10">\n' +
                     '<h2 class="post-title"><a href="">a<\/a></h2>\n' +
                     '<span class="post-author"><a href="">Post Author<\/a></span>\n' +
                     '<span class="post-published">Published <span class="date"></span> at <span class="time"></span><\/span>\n' +
                     '<span class="post-link"><a href="">View on Flickr<\/a></span>\n' +
-                    '<\/div>' +
+                    '<\/div>\n' +
                     '</li>';
 
-var thinText =  '<li class="feedItem">\n' +
-                    '<div class="post-image col-3">' +
-                        '<img  src="img/Connor.png" alt="No Image" />' +
-                    '</div>' +
-                    '<div class="post-details col-10">' +
-                        '<h2 class="post-title"><a href=""></a></h2>' +
-                    '<span class="post-published">Published <span class="date"></span> at <span class="time"></span><\/span>\n' +
-                        '<span class="post-author"><a href="">Post Author</a></span>' +
-                        '<span class="post-link"><a href="">View on Flickr</a></span>' +
-                    '</div>' +
-                '</li>';
+var thinText =      '<li class="feedItem row">\n' +
+                    '<div class="post-image col-2">\n' +
+                    '<a href=""><img src="" alt="No Image" /></a>\n' +
+                    '</div>\n' +
+                    '<div class="post-details col-10">\n' +
+                    '<h2 class="post-title"><a href=""></a></h2>\n' +
+                    '<span class="post-published">Published <span class="date"></span> at <span class="time"></span><\/span><br />\n' +
+                    '<span class="post-author"><a href="">Post Author</a></span>\n' +
+                    '<span class="post-link"><a href="">View on Flickr</a></span>\n' +
+                    '</div>\n' +
+                    '</li>';
 
 function retrieveData() {
+    var width = $(window).width();
     var url = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?&tags=space&tagmode=all&format=json";
     $.getJSON(url, {}, function(data) {
         var items = data.items;
         $.each(items, function(index, value) {
-//            alert("test");
             var newIndex = parseInt(index) + 1;
-            $("#feed ul").append(wideText);
-            $("#feed ul li:nth-child(" + newIndex + ") img").attr("src", value["media"]["m"]);
+            if(width < 800) {
+                $("#feed ul").append(thinText);
+                $(".row").css("height", "40vh");
+            } else {
+                $("#feed ul").append(wideText);
+            }
             var postLink = "";
             /* Set variable as link for post. */
+            $("#feed ul li:nth-child(" + newIndex + ") img").attr("src", value["media"]["m"]);
+            $("#feed ul li:nth-child(" + newIndex + ") a").attr("href", postLink);
             $("#feed ul li:nth-child(" + newIndex + ") .post-title a").text(value["title"]).attr("href", postLink);
             var author = "https://www.flickr.com/photos/" + value['author_id'];
             $("#feed ul li:nth-child(" + newIndex + ") .post-author a").attr("href", author);
